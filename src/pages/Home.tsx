12388c7b7ececc882238e12a5c1968cd13311a66
ericxlima <eric.vinlima@gmail.com>
2024-07-05 02:44:03 -0300

    feat: add start and stop scam

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonContent, IonHeader, IonPage, IonRow, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
 import { useEffect, useState } from 'react';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
@@ -7,6 +7,22 @@ import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 const Home: React.FC = () => {
 
   const [err, seterr] = useState<string>()
+
+  const startScan = async () => {
+    await BarcodeScanner.checkPermission({ force: true });
+    BarcodeScanner.hideBackground();
+  
+    const result = await BarcodeScanner.startScan();
+
+    if (result.hasContent) {
+      console.log(result.content);
+    }
+  };
+
+  const stopScan = () => {
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+  };
 
   useEffect(() => {
     const checkPermission = async () => {
@@ -28,17 +44,37 @@ const Home: React.FC = () => {
     }
   }, [])
 
+  if (err){
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Comanda</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className='ion-padding'>
+          <IonRow>
+            <IonText color="danger">{err}</IonText>
+          </IonRow>
+        </IonContent>
+      </IonPage>
+    );
+  }
+  
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Comanda</IonTitle>
+          <IonButtons slot='end'>
+            <IonButton onClick={stopScan}> Stop Scan</IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
-      <IonContent className='ion-padding'>
-        <IonRow>
-          {err}
-        </IonRow>
+      <IonContent className='hideBg'>
+        <IonButton onClick={startScan}>
+          Start Scam
+        </IonButton>
       </IonContent>
     </IonPage>
   );
