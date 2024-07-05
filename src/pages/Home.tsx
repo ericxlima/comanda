c2ee9c5fbcb9e11b3966f8f60733bcb56dab14de
ericxlima <eric.vinlima@gmail.com>
2024-07-05 02:22:17 -0300

    feat: add check permissions module to camera

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,44 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
+import { useEffect, useState } from 'react';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+
 
 const Home: React.FC = () => {
+
+  const [err, seterr] = useState<string>()
+
+  useEffect(() => {
+    const checkPermission = async () => {
+      try {
+        const status = await BarcodeScanner.checkPermission({ force: true });
+        if (status.granted) {
+          return true;
+        }
+        return false;
+      } catch (error) {
+        seterr(error.message)
+      }
+    };
+
+    checkPermission()
+    
+    return () => {
+
+    }
+  }, [])
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Comanda</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className='ion-padding'>
+        <IonRow>
+          {err}
+        </IonRow>
       </IonContent>
     </IonPage>
   );
